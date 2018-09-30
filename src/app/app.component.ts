edcf56a5d7b73acabf8e4f951ebd039ee3c1d138
Paul Spears <paul.spears@oasisdigital.com>
2018-09-30 08:03:32 -0500

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,18 @@ interface Record {
   units: number;
   totalRevenue: number;
   rank: number;
+  region: string;
 }
 
 const recordList: Record[] = [
-  { empName: 'Cynthia Cunningham', units: 4575.29879815, totalRevenue:  7746739.8278,   rank: 7 },
-  { empName: 'Peter Clark',        units: 1030,          totalRevenue:  6448675.309,    rank: 8 },
-  { empName: 'Theresa Soto',       units: 8669.89,       totalRevenue: 37439820.3498,   rank: 2 },
-  { empName: 'Russell Fisher',     units: 4565.3892,     totalRevenue: 15789230.9843,   rank: 4 },
-  { empName: 'Elizabeth Hudson',   units: 1589.2979879,  totalRevenue:  9789709.834098, rank: 5 },
-  { empName: 'Heather Spencer',    units: 9980,          totalRevenue: 29879873.40598,  rank: 3 },
-  { empName: 'Barbara Tran',       units: 3038.6829,     totalRevenue: 49955398.2987,   rank: 1 },
-  { empName: 'Julia Anderson',     units: 3928.593,      totalRevenue:  8678098.979998, rank: 6 }
+  { empName: 'Cynthia Cunningham', units: 4575.29879815, totalRevenue:  7746739.8278,   rank: 7, region: 'central' },
+  { empName: 'Peter Clark',        units: 1030,          totalRevenue:  6448675.309,    rank: 8, region: 'central' },
+  { empName: 'Theresa Soto',       units: 8669.89,       totalRevenue: 37439820.3498,   rank: 2, region: 'central' },
+  { empName: 'Russell Fisher',     units: 4565.3892,     totalRevenue: 15789230.9843,   rank: 4, region: 'eastern' },
+  { empName: 'Elizabeth Hudson',   units: 1589.2979879,  totalRevenue:  9789709.834098, rank: 5, region: 'eastern' },
+  { empName: 'Heather Spencer',    units: 9980,          totalRevenue: 29879873.40598,  rank: 3, region: 'eastern' },
+  { empName: 'Barbara Tran',       units: 3038.6829,     totalRevenue: 49955398.2987,   rank: 1, region: 'western' },
+  { empName: 'Julia Anderson',     units: 3928.593,      totalRevenue:  8678098.979998, rank: 6, region: 'western' }
 ];
 
 @Component({
@@ -28,13 +29,8 @@ export class AppComponent {
   unitsScale   = 100 / 10000;
   revenueScale = 100 / 50000000;
 
-  status(rank: number): string {
-    if (rank === 1) {
-      return 'bonus';
-    } else if (rank >= 7) {
-      return 'fired';
-    } else {
-      return '';
-    }
+  // Avoid computation in templates
+  regionClass(r: Record) {
+    return 'region-' + r.region;
   }
 }
